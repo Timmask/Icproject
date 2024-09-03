d3d3de140c07d1f3594d42346ee15f685aaf294e
Timmask <timakhamza@gmail.com>
2024-09-03 12:31:06 +0500

    azure add 1.1

--- src/main/java/com/temirlan/spring/mvc/icproject/HttpLoggingFilter.java
+++ src/main/java/com/temirlan/spring/mvc/icproject/HttpLoggingFilter.java
@@ -1,15 +1,20 @@
 package com.temirlan.spring.mvc.icproject;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import java.net.Inet4Address;
 
 public class HttpLoggingFilter extends AbstractRequestLoggingFilter {
 
 
+    @SneakyThrows
     @Override
     protected void beforeRequest(HttpServletRequest request, String message) {
         System.out.println(request.getRequestURL());
         System.out.println(message);
+        System.out.println(Inet4Address.getLocalHost().getHostAddress());
     }
 
     @Override
